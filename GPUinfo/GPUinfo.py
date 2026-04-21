5f9c40cfc7c1042150ead5b7cb05cf885ea5cd68
gindvd <gingerichdvd@gmail.com>
2026-04-21 17:25:41 +0900

    fix: Change function to get manufacturers names

--- GPUinfo/GPUinfo.py
+++ GPUinfo/GPUinfo.py
@@ -5,20 +5,20 @@ David Gingerich
 Version 1.0.0
 """
 
-from GPUinfo.gpuinfo_handler import *
+from handler import *
 
-def names() -> list[str]:
-  output = get_gpu_names()
-  
-  cleaned_list = clean_data(output.splitlines())
-  gpu_fullnames = [item.split(": ")[1] for item in cleaned_list]
+def get_manufacturers():
+  device_info = get_gpu_names()
+  print(device_info)
 
-  return gpu_fullnames
+  items = device_info.splitlines()
 
-def manufacturers() -> list[str]:
- 
-  gpu_fullnames = names()
-  gpu_splitnames = [item.split() for item in gpu_fullnames]
-  
-  vendor_names = [item[0] for item in gpu_splitnames]
-  return vendor_names
+  manufacturers = []
+  for item in items:
+    temp = []
+    item = remove_symbols(item)
+    temp = item.split()
+
+    manufacturers.append(temp[0])
+
+  return manufacturers
